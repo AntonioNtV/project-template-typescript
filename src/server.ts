49f67d74dae7ccde61e2863910cd86c894f913f4
AntonioNtV <antoniontv18@gmail.com>
2020-08-25 08:20:01 -0300

    Configuraçao inicial

---
+++ src/server.ts
@@ -0,0 +1,14 @@
+import express from 'express'
+
+const app = express()
+
+app.get('/', (request, response) => {
+    return response.json({
+        message: 'Hello World! 🌎'
+    })
+})
+
+app.listen(3333, () => {
+    console.log('🚀 Server started on port 3333');
+    
+})
